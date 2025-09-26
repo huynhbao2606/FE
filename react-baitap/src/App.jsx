a8159e5187406a84efb9ff87d5d9f0a6126188c4
HuynhBao <huynhbao0626@gmail.com>
2025-09-26 21:20:09 +0700

    config

---
+++ react-baitap/src/App.jsx
@@ -0,0 +1,13 @@
+import DatVe from "./dat-ve/index.jsx";
+import "./styles/datve.css"
+import 'flowbite';
+function App() {
+
+  return (
+    <>
+        <DatVe/>
+    </>
+  )
+}
+
+export default App
